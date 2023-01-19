06e8909a9b451a314fc09a78d8f5f9c3edf0d7d0
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-19 14:02:54 -0300

    2 files modified

--- clase8DOM.js
+++ clase8DOM.js
@@ -8,3 +8,18 @@ let paises = document.getElementsByClassName("paises");
 console.log(paises[0].innerHTML);
 console.log(paises[1].innerHTML);
 console.log(paises[2].innerHTML);
+
+let contenedores = document.getElementsByTagName("div");
+console.log(contenedores[0].innerHTML);
+console.log(contenedores[1].innerHTML);
+console.log(contenedores[2].innerHTML);
+
+for (const pais of paises) {
+  console.log(pais.innerHTML);
+}
+
+for (const div of contenedores) {
+  console.log(div.innerHTML);
+}
+
+
