628c311b84cc5771ca247effb6b1519be445aeb7
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-19 14:53:01 -0300

    2 files modified

--- clase8DOM.js
+++ clase8DOM.js
@@ -47,3 +47,15 @@ paises[0].remove()
 
 document.getElementById("nombre").value = "HOMERO";
 document.getElementById("edad").value   = 39;
+
+//Obtenemos el nodo donde vamos a agregar los nuevos elementos
+let padre = document.getElementById("personas");
+//Array con la información a agregar
+let personas = ["HOMERO","MARGE", "BART", "LISA","MAGGIE"];
+//Iteramos el array con for...of
+for (const persona of personas) {
+    //Creamos un nodo <li> y agregamos al padre en cada ciclo
+    let li = document.createElement("li");
+    li.innerHTML = persona
+    padre.appendChild(li);
+}
