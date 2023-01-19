bae10c6ce12c1aea2b6fd270aaae84b1cf6d78a3
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-19 13:43:45 -0300

    1 file modified, 1 added

---
+++ clase8DOM.js
@@ -0,0 +1,10 @@
+let app     = document.getElementById("app");
+let parrafo = document.getElementById("parrafo1");
+// innerHTML toma el contenido interno del nodo, se puede usar para acceder al contenido o modificalo
+console.log(app.innerHTML);
+console.log(parrafo.innerHTML);
+//CODIGO JS
+let paises = document.getElementsByClassName("paises");
+console.log(paises[0].innerHTML);
+console.log(paises[1].innerHTML);
+console.log(paises[2].innerHTML);
