58214a172fca1215c01f433aae5fce596d965dc0
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-26 21:32:50 -0300

    1 file modified

--- clase14Promesas.js
+++ clase14Promesas.js
@@ -1,3 +1,4 @@
+/*
 setTimeout(()=> {
   console.log("Proceso asincrónico")
 }, 3000)
@@ -23,3 +24,39 @@ btn.addEventListener('click', () => {
         popup.classList.remove('popup-active')
     }, 2500)
 })
+*/
+/*
+for (let letra of "hola") {
+  console.log(letra)
+}
+
+
+for (let letra of "mundo") {
+  console.log(letra)
+}
+
+for (let letra of "hola") {
+  setTimeout(() => {
+      console.log(letra)
+  }, 1000)
+}
+
+
+for (let letra of "mundo") {
+  setTimeout(() => {
+      console.log(letra)
+  }, 3000)
+}
+*/
+//Sabiendo cómo funciona el motor de Javascript para organizar las tareas sincrónicas y asincrónicas, queda más claro cómo es posible que una instrucción como la siguiente tiene  el resultado que tiene:
+
+console.log("Inicia proceso")
+
+
+setTimeout(()=> {
+    console.log("Mitad de proceso")
+}, 0)
+
+
+console.log("Fin proceso")
+
