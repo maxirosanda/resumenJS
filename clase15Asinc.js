657d7c10d1ed87538d146f7f95c5543d205451be
maxirosanda <75870261+maxirosanda@users.noreply.github.com>
2023-01-27 21:14:34 -0300

    1 file modified, 1 added

---
+++ clase15Asinc.js
@@ -0,0 +1,5 @@
+//Cada petición tiene un Status, que es un código que significa el estado de la petición. 
+//Códigos de estado que comienzan en 2 (generalmente 200), significa que la petición fue exitosa.
+//Los códigos que empiezan con 4 significan que hubo algún error con la petición. El famoso 404 indica que el recurso buscado no fue encontrado.
+//Códigos que empiezan con 5 significan que hubo un error con el servidor, por lo tanto no es culpa de la petición.
+
